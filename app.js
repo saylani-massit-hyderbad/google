43bace0ea9a9c7f253e6b716295232930fdae129
saylani_mass_it_hyderabad <82083475+saylani-massit-hyderbad@users.noreply.github.com>
2021-10-16 13:13:37 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function gsignin(){
     var provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider)
     .then((res)=>{
-        console.log(res)
+        console.log(res.user)
     })
     .catch((err)=>{
         console.log(err)
