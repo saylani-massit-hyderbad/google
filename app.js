33cd51f23f46835be877fc562c608a72f84ba922
saylani_mass_it_hyderabad <82083475+saylani-massit-hyderbad@users.noreply.github.com>
2021-10-17 15:54:48 +0500

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ function fblogin(){
     var provider = new firebase.auth.FacebookAuthProvider();
     firebase.auth().signInWithPopup(provider)
     .then((data)=>{
-        console.log(data)
+        var user = data.user
+        console.log(user.displayName)
+        console.log(user.photoURL)
+        console.log(user.email)
     })
     .catch((err)=>{
         console.log(err)
